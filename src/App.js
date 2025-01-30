35241214dd4dd9b6d2de6fd8ac3cd4255db53138
ElainneCris1409 <crisnetwork14@gmail.com>
2025-01-30 22:00:22 +0000

    docs: dependencias estudo frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        
+          <div>Estou aqui:<input></input></div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
